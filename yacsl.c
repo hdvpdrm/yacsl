154c243fe7e0c2fb11c270f1469e2121c0353054
MAGANER <maganer.krik@mail.ru>
2023-10-15 13:51:46 +0300

    check if passed pointer is null

--- yacsl.c
+++ yacsl.c
@@ -2,6 +2,11 @@
 
 int sl_init(string_t* str, size_t len)
 {
+	if(str == NULL)
+	{
+		return SL_FAIL;
+	}
+
 	str->buffer = (char*)malloc(len*sizeof(char));
 	if(str->buffer == NULL)
 		return SL_FAIL;
@@ -13,6 +18,11 @@ int sl_init(string_t* str, size_t len)
 }
 int sl_fill(string_t* str, char* val)
 {
+	if(str == NULL)
+	{
+		return SL_FAIL;
+	}
+
 	//decrement len, because strlen don't count the terminator null
 	if(strlen(val) > str->len-1)
 	{
@@ -28,6 +38,12 @@ int sl_fill(string_t* str, char* val)
 }
 int sl_filln(string_t* str, char* val, size_t n)
 {
+	if(str == NULL || 
+	   val == NULL)
+	{
+		return SL_FAIL;
+	}
+
 	if(strlen(val)+n > str->len-1)
 	{
 		return SL_FAIL;
@@ -48,3 +64,28 @@ int sl_eq(string_t* a, string_t* b)
 {
 	return strcmp(a->buffer,b->buffer);
 }
+
+int sl_cat(string_t* a, string_t* b)
+{
+	if(a->buffer == NULL || 
+	   b->buffer == NULL)
+	{
+		return SL_FAIL;
+	}
+	a->buffer = (char*)realloc(a->buffer,b->len*sizeof(char));
+
+	if(a->buffer == NULL)
+	{
+		return SL_FAIL;
+	}
+	a->len = a->len + b->len;
+
+	size_t n = 0;
+	for(size_t i = a->len-1;i<a->len;++i,++n)
+	{
+		a->buffer[i]=b->buffer[n];
+	}
+
+	return SL_OK;
+}
+
